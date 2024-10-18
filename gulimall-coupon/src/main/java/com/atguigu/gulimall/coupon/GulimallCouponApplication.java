6aedd07ba3140b1c914026b8602d3634443d19ce
‘thehou’ <1505748632@qq.com>
2024-10-18 16:16:30 +0800

    feat:springcloud相关组件使用

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -2,7 +2,11 @@ package com.atguigu.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.PropertySource;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallCouponApplication {
 
--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
@@ -3,7 +3,10 @@ package com.atguigu.gulimall.coupon.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +29,27 @@ import com.atguigu.common.utils.R;
  */
 @RestController
 @RequestMapping("coupon/coupon")
+@RefreshScope
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.name}")
+    private String name;
+    @Value("${coupon.age}")
+    private int age;
+
+    @RequestMapping("/test")
+    public R test() {
+        return R.ok().put("name", name).put("age", age);
+    }
+
+    @RequestMapping("/member/list")
+    public R membercopons() {
+        CouponEntity couponEntity = new CouponEntity();
+        couponEntity.setCouponName("慢100-10");
+        return R.ok().put("coupon", couponEntity);
+    }
 
     /**
      * 列表
