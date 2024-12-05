098300eba4ef0e7697b40be4fc32f0e17dd2521e
‘thehou’ <1505748632@qq.com>
2024-12-05 21:55:22 +0800

    feat:商品服务-获取当前分类下所有分组以及属性

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrGroupServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/AttrGroupServiceImpl.java
@@ -3,20 +3,32 @@ package com.atguigu.gulimall.product.service.impl;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.Query;
 import com.atguigu.gulimall.product.dao.AttrGroupDao;
+import com.atguigu.gulimall.product.entity.AttrEntity;
 import com.atguigu.gulimall.product.entity.AttrGroupEntity;
 import com.atguigu.gulimall.product.service.AttrGroupService;
+import com.atguigu.gulimall.product.vo.AttrGroupWithAttrsVo;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("attrGroupService")
 public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEntity> implements AttrGroupService {
 
+
+    private final AttrServiceImpl attrService;
+
+    public AttrGroupServiceImpl(AttrServiceImpl attrService) {
+        this.attrService = attrService;
+    }
 
     @Override
     public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
@@ -38,6 +50,29 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 获取当前分类下所有分组以及属性
+     * @param catelogId
+     * @return
+     */
+    @Override
+    public List<AttrGroupWithAttrsVo> getAttrGroupWithAttrsByCatelogId(Long catelogId) {
+        if (catelogId == null) {
+            return new ArrayList<>();
+        }
+        List<AttrGroupEntity> attrGroupEntities = this.list(
+                new LambdaQueryWrapper<AttrGroupEntity>().eq(AttrGroupEntity::getCatelogId, catelogId));
+        List<AttrGroupWithAttrsVo> data = attrGroupEntities.stream().map(entity -> {
+            AttrGroupWithAttrsVo attrGroupWithAttrsVo = new AttrGroupWithAttrsVo();
+            BeanUtils.copyProperties(entity, attrGroupWithAttrsVo);
+            Long attrGroupId = entity.getAttrGroupId();
+            List<AttrEntity> attrs = attrService.getRelationAttr(attrGroupId);
+            attrGroupWithAttrsVo.setAttrs(attrs);
+            return attrGroupWithAttrsVo;
+        }).collect(Collectors.toList());
+        return data;
     }
 
 }
